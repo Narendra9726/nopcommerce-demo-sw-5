c130ef99bd4a2e25e0df11eca967ef02aca790db
Narendra9726 <info.dhaduk@gmail.com>
2023-12-12 19:28:07 +0000

    nopcommerce-demo-sw-5

---
+++ src/test/java/com/nopcommerce/demo/testbase/BaseTest.java
@@ -0,0 +1,23 @@
+package com.nopcommerce.demo.testbase;
+
+import com.nopcommerce.demo.utilities.Utility;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+/**
+ * Created by Jay Vaghani
+ */
+public class BaseTest extends Utility {
+
+    String browser = com.nopcommerce.demo.propertyreader.PropertyReader.getInstance().getProperty("browser");
+
+    @BeforeMethod
+    public void setUp(){
+        selectBrowser(browser);
+    }
+
+    @AfterMethod
+    public void tearDown(){
+        closeBrowser();
+    }
+}
